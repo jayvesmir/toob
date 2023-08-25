9c34a044a22b9781c4ed55f9a69351ca278f662e
jayvesmir <marceleenah@gmail.com>
2023-08-25 10:20:06 +0200

    handle file error

--- src/file.cpp
+++ src/file.cpp
@@ -1,9 +1,12 @@
 #include "file.hpp"
+#include <format>
 #include <fstream>
 #include <iterator>
 
 std::string file::readEntire(const std::string& _filename) {
     std::ifstream stream(_filename);
+    if (!stream)
+        throw std::runtime_error(std::format("couldn't open {}", _filename));
     return std::string(std::istreambuf_iterator<char>(stream),
                        std::istreambuf_iterator<char>());
 }
--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,19 @@
 #include <format>
 #include <iostream>
 #include <stdexcept>
+#include <string>
+#include <vector>
 
 int32_t main(int argc, char** argv) {
+    if (argc < 2) {
+        std::cout << std::format("usage:\n\t{} <input_filename>\n", argv[0]);
+        return -1;
+    }
+    std::vector<std::string> args(argv, argv + argc);
+
     try {
-        std::string fileContents = file::readEntire(argv[1]);
+        // assume first argument to be the filename
+        std::string fileContents = file::readEntire(args[1]);
         parser::generateParseTree(fileContents);
     } catch (const std::exception& e) {
         std::cout << std::format("[exception] {}", e.what());
